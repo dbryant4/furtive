7159e4102ef08aeb74fc5d46fa6f5570b89a5618
Derrick Bryant <dbryant4@gmail.com>
2015-12-02 17:09:59 -0800

    Change assertion to work with 2.6

--- tests/test_manifest.py
+++ tests/test_manifest.py
@@ -34,7 +34,7 @@ class TestManifest(unittest.TestCase):
         self.test_manifest_save()
 
         manifest = Manifest('test-data', '.test_manifest.db')
-        self.assertIs(manifest.manifest, None)
+        self.assertTrue(manifest.manifest ish None)
         manifest.load()
 
         print manifest.manifest
