b5e948e0c35528539800dc7994434b101e339c9d
Derrick Bryant <dbryant4@gmail.com>
2015-12-07 21:07:22 -0800

    Remove references to other projects

--- docs/conf.py
+++ docs/conf.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 #
-# community_tools documentation build configuration file, created by
+# furtive documentation build configuration file, created by
 # sphinx-quickstart on Sun Aug 23 11:25:58 2015.
 #
 # This file is execfile()d with the current directory set to its
@@ -205,7 +205,7 @@ html_static_path = ['_static']
 #html_search_scorer = 'scorer.js'
 
 # Output file base name for HTML help builder.
-htmlhelp_basename = 'community_toolsdoc'
+htmlhelp_basename = 'furtivedoc'
 
 # -- Options for LaTeX output ---------------------------------------------
 
@@ -227,7 +227,7 @@ latex_elements = {
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-  (master_doc, 'community_tools.tex', u'community\\_tools Documentation',
+  (master_doc, 'furtive.tex', u'Furtive Documentation',
    u'Derrick Bryant', 'manual'),
 ]
 
@@ -257,7 +257,7 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    (master_doc, 'community_tools', u'community_tools Documentation',
+    (master_doc, 'furtive', u'Furtive Documentation',
      [author], 1)
 ]
 
@@ -271,8 +271,8 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-  (master_doc, 'community_tools', u'community_tools Documentation',
-   author, 'community_tools', 'One line description of project.',
+  (master_doc, 'Furtive', u'Furtive Documentation',
+   author, 'furtive', 'File Integrity Verification System.',
    'Miscellaneous'),
 ]
 
