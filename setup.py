2e8530271524d949ec5a50a14fb83043914f422b
Derrick Bryant <dbryant4@gmail.com>
2015-12-18 11:23:37 -0800

    Correct username in travis config

--- setup.py
+++ setup.py
@@ -2,11 +2,15 @@
 
 from distutils.core import setup
 
+with open('README.md') as file:
+    long_description = file.read()
+
 setup(name='Furtive',
       version='0.2',
       description='File Integrity Verification System',
       author='Derrick Bryant',
       author_email='dbryant4@gmail.com',
+      long_description=long_description,
       packages=['furtive'],
       scripts=['scripts/furtive'],
       install_requires=[
