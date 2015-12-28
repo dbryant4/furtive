224bf8a00a10a7e99fa73c6c631f1dedcc5ac25f
Derrick Bryant <dbryant4@gmail.com>
2015-12-28 13:56:12 -0600

    Add more python versions to testing framework

--- setup.py
+++ setup.py
@@ -27,5 +27,14 @@ setup(name='Furtive',
         'PyYAML==3.11',
         'argparse==1.4.0',
         'future==0.15.2'
-      ]
+      ],
+      classifiers=[
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+      ],
      )
